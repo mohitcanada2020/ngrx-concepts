d12e0cd0ac6c06eb36cffb154fa690dc49ffc7dc
Mohit <mohitcanada2020@gmail.com>
2024-01-27 15:27:40 -0500

    Errors resolved

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { CounterComponent } from './components/counter/counter.component';
+import { BlogComponent } from './components/blog/blog.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component:HomeComponent
+  },
+  {
+    path:'counter',
+    component:CounterComponent
+  },
+  {
+    path:'blog',
+    component:BlogComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,49 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CounterbuttonComponent } from './components/counterbutton/counterbutton.component';
+import { CounterdisplayComponent } from './components/counterdisplay/counterdisplay.component';
+import { StoreModule } from '@ngrx/store';
+import { counterReducer } from './shared/store/counter.reducer';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './Material.module';
+import { CustomcounterComponent } from './components/customcounter/customcounter.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { HomeComponent } from './components/home/home.component';
+import { CounterComponent } from './components/counter/counter.component';
+import { BlogComponent } from './components/blog/blog.component';
+import { HeaderComponent } from './components/header/header.component';
+import { AppState } from './shared/store/Global/App.state';
+import { HttpClientModule } from '@angular/common/http';
+import { AddblogComponent } from './components/addblog/addblog.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CounterbuttonComponent,
+    CounterdisplayComponent,
+    CustomcounterComponent,
+    HomeComponent,
+    CounterComponent,
+    BlogComponent,
+    HeaderComponent,
+    AddblogComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MaterialModule,
+    StoreModule.forRoot(AppState),
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    StoreDevtoolsModule.instrument({ maxAge: false, logOnly: !isDevMode() }),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
